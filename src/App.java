878143c645ffe2928d872ef91c61819436a53206
Lucas Lafèré <laferelucas@gmail.com>
2023-01-09 17:58:47 -0300

    initial studies

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String name = "Lucas";
+        Integer age = 24;
+        String output = "Meu nome é " + name + " e eu tenho " + age + " anos de idade";
+        System.out.println(output);
+    }
+}
